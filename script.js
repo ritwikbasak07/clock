cfd09a33885743051d92813df1864d8aa9f79d66
Ritwik <ritwikbasak07@gmail.com>
2022-08-24 14:19:50 +0530

    dark & light theme introduce

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
 
-
-const checkBox = document.querySelector("#accept");
-
-let check = checkBox.checked;
-
-checkBox.addEventListener("change", (e) =>{
-    if(e.target.checked){
-        englishBefore();
-       hindi();
-    }else{
-        hindiBefore();
-       english();
-    }
-})
-
-function hindi(){
+function hindi() {
     let cssc = document.querySelector(".hindi");
-    
+
     cssc.style.transition = "0.2s";
     cssc.style.fontSize = "25px";
     cssc.style.margin = "0px 0px 0px 7.3px";
 }
-function hindiBefore(){
+function hindiBefore() {
     let cssc = document.querySelector(".hindi");
     cssc.style.fontSize = "20px";
     cssc.style.margin = "2.5px 10px";
 }
 
-function english(){
+function english() {
     let cssc = document.querySelector(".eng");
     cssc.style.transition = "0.2s";
     cssc.style.fontSize = "25px";
     cssc.style.margin = "0px 7.3px 0px 0px";
 }
-function englishBefore(){
+function englishBefore() {
     let cssc = document.querySelector(".eng");
     cssc.style.fontSize = "20px";
     cssc.style.margin = "2.5px 10px";
 }
 
+let check;
+
+let checkBox = document.querySelector("#accept");
+checkBox.addEventListener("change", (e) => {
+    check = e.target.checked;
+    if (check) {
+        englishBefore();
+        hindi();
+    } else {
+        hindiBefore();
+        english();
+    }
+})
 
 function updateClock() {
-    const currentDate = new Date();
-    const hour = currentDate.getHours();
-    const minute = currentDate.getMinutes();
-    const second = currentDate.getSeconds();
-    const hourDeg = ((hour / 12) * 360) + ((minute / 60) * 30) + ((second / 60) * .5);
+    let a = 54;
+    let currentDate = new Date();
+    let hour = currentDate.getHours();
+    let minute = currentDate.getMinutes()+a;
+    let second = currentDate.getSeconds();
+    hourDeg = ((hour / 12) * 360) + ((minute / 60) * 30) + ((second / 60) * .5);
     document.querySelector(".hour").style.transform = `rotate(${hourDeg}deg)`;
-    const minuterDeg = ((minute / 60) * 360) + ((second / 60) * 6);
+    minuterDeg = ((minute / 60) * 360) + ((second / 60) * 6);
     document.querySelector(".minute").style.transform = `rotate(${minuterDeg}deg)`;
-    const secondDeg = (second / 60) * 360;
+    secondDeg = (second / 60) * 360;
     document.querySelector(".second").style.transform = `rotate(${secondDeg}deg)`;
-    setTimeout(updateClock, 1000);
+setTimeout(updateClock, 1000);
+
+
 }
+
+
 updateClock();
+
 
